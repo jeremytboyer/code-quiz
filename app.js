31ea9f1095d355c2b82a8368edaab90e7513c345
Jeremy Boyer <jeremytboyer@gmail.com>
2023-06-07 21:22:25 -0400

    updated code

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
 var startBtn = document.querySelector("#start-btn");
 var currentQuestionIndex = 0;
+var wrongQuestions = [];
+var div = document.querySelector("div");
+var h2 = document.querySelector("h2");
 
-function showCurrentQuestion(index) {
+
+function showCurrentQuestion() {
   // write a function to get the current question index and display that message in the h2
-  var h2 = document.querySelector("h2");
 
   h2.innerText = questionData[currentQuestionIndex].question;
 
   //write a loop that displays the questions as buttons
-  var choices = questionData[currentQuestionIndex].choices
-  var div = document.querySelector('div')
+  var choices = questionData[currentQuestionIndex].choices;
+  
+  div.innerHTML = "";
   choices.forEach((choice) => {
     var btn = document.createElement("button");
     div.appendChild(btn);
     btn.innerText = choice;
   });
 
-  var btns = document.querySelectorAll('button')
+  var btns = document.querySelectorAll("button");
 
   function handleClick(e) {
-    console.log(e.target.innerText)
-    ++currentQuestionIndex
-    console.log(currentQuestionIndex)
+    if (e.target.innerText !== questionData[currentQuestionIndex].answer) {
+      wrongQuestions.push(questionData[currentQuestionIndex].question);
+    }
+    currentQuestionIndex++;
+    console.log(currentQuestionIndex);
+    showCurrentQuestion();
   }
 
-  btns.forEach(btn => {
-    btn.addEventListener('click', handleClick)
-  })
+  btns.forEach((btn) => {
+    btn.addEventListener("click", handleClick);
+  });
+}
+
+function endGame() {
+  if (currentQuestionIndex > questionData.length) {
+    div.innerHTML = "congrats";
+  }
 }
 
 function startGame() {
   startBtn.classList.add("hide");
-      showCurrentQuestion();
+  showCurrentQuestion();
+  endGame();
 }
 
 startBtn.addEventListener("click", startGame);
